a7751f874ac94cd7271dcc46b15f796a29594446
muhadhilr <fadhilrmdhn3010@gmail.com>
2025-07-15 00:28:51 +0700

    fix: minor changes in news section

--- src/feature/schedule/components/section/hero-section.tsx
+++ src/feature/schedule/components/section/hero-section.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
     <section className="px-8 lg:px-24 py-5">
       <div className="text-xl font-thin space-y-2">
         <p>{`Worship Schedule`.toUpperCase()}</p>
-        <h1 className="text-4xl font-bold pb-10">Misa Gereja</h1>
+        <h1 className="text-4xl font-bold pb-10">Sunday Service</h1>
       </div>
       <div className="space-y-5">
         {ScheduleList.map((item, index) => (
